76614ef5fb6a1bc4f786ab0d2f809a31474d6d5b
kiisi <kiisifelix06@gmail.com>
2022-10-04 23:37:16 +0100

    Update on typography

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <div className="about">
             <div className="about-content">
                 <h1>About Us</h1>
-                <p>Urban Logistics Delivery Services is one of the leading providers of courier services, sea freight and air freight services in the world. Our primary focus is to ensure that our customers receive their items at the desired destination at the desired time.</p>
+                <p>Speedway Movers Delivery Company is one of the leading providers of courier services, sea freight and air freight services in the world. Our primary focus is to ensure that our customers receive their items at the desired destination at the desired time.</p>
                 <p>We proud ourselves so high as one of the best delivery company in the world, due to international recognitions and awards we have gotten so far.</p>
             </div>
             <div className="about-img">
