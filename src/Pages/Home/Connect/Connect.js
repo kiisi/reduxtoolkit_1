e252351c14ba03050dfefe29496eff6458b2f54e
kiisi <kiisifelix06@gmail.com>
2022-10-04 23:52:06 +0100

    Added connect component

---
+++ src/Pages/Home/Connect/Connect.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Connect.css'
+import { ReactComponent as Intersection } from '../../../assets/Intersection-2.svg'
+import { ReactComponent as Intersect } from '../../../assets/Intersection-1.svg'
+
+const Connect = () => {
+  return (
+    <div className="max-wrapper connect-wrapper" id="connect">
+      <div className="connect">
+        <div className="connect-intersection">
+          <Intersection />
+        </div>
+        <div className="connect-intersect">
+          <Intersect />
+        </div>
+        <div className="connect-content">
+          <div className="connect-content-line"></div>
+          <h1 className="connect-content-main">Let's Work Together</h1>
+          <div className="connect-content-sub">Contact us by clicking on the button below</div>
+          <div className="connect-content-btn">
+            <div className="btn-c">                                
+            <a href="mailto:speedwaysinternationaldelivery@aol.com">Contact Us</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Connect
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from './About/About'
+import Connect from './Connect/Connect'
 import Hero from './Hero/Hero'
 import Offer from './Offer/Offer'
 import Offersub from './Offersub/Offersub'
@@ -13,6 +14,7 @@ const Home = () => {
       <Offersub/>
       <About/>
       <Testimony/>
+      <Connect/>
     </>
   )
 }
